c472c1c68e4aeb1be214493acc03393e17d338e1
SaruulbayarB <saruuleagle@gmail.com>
2025-08-06 07:52:07 +0800

    first push

---
+++ index.js
@@ -0,0 +1,16 @@
+import _ from "lodash";
+
+const max = _.max([2, 4, 5, 9]);
+
+console.log(max);
+
+// Example 1:
+
+let s = "Hello World";
+
+let counter = 0;
+var lengthOfLastWord = function (s) {
+  for (i = s.length - 1; i >= 0; i--) {}
+};
+
+console.log(s[i]);
